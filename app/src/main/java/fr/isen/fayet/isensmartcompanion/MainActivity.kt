8c39216acce210515d00e030892d5bb881770249
fabio <fabio.fayet@isen.yncrea.fr>
2025-02-24 17:54:39 +0100

    Part 1 & Part 2.1

--- app/src/main/java/fr/isen/fayet/isensmartcompanion/MainActivity.kt
+++ app/src/main/java/fr/isen/fayet/isensmartcompanion/MainActivity.kt
@@ -4,44 +4,82 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import fr.isen.fayet.isensmartcompanion.screen.MainScreen
+import fr.isen.fayet.isensmartcompanion.screen.TabView
 import fr.isen.fayet.isensmartcompanion.ui.theme.ISENSmartCompanionTheme
+
+data class TabBarItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val badgeAmount: Int? = null
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            // setting up the individual tabs
+            val homeTab = TabBarItem(title = "Home", selectedIcon = Icons.Filled.Home, unselectedIcon = Icons.Outlined.Home)
+            val alertsTab = TabBarItem(title = "Alerts", selectedIcon = Icons.Filled.Notifications, unselectedIcon = Icons.Outlined.Notifications, badgeAmount = 7)
+            val settingsTab = TabBarItem(title = "Settings", selectedIcon = Icons.Filled.Settings, unselectedIcon = Icons.Outlined.Settings)
+
+            // creating a list of all the tabs
+            val tabBarItems = listOf(homeTab, alertsTab, settingsTab)
+
+            // creating our navController
+            val navController = rememberNavController()
+
             ISENSmartCompanionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Scaffold(bottomBar = { TabView(tabBarItems, navController) },
+                    modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(Modifier.padding(innerPadding)) {
+                        NavHost(navController = navController, startDestination = homeTab.title) {
+                            composable(homeTab.title) {
+                                MainScreen(innerPadding)
+                            }
+                            composable(alertsTab.title) {
+                                Text(alertsTab.title)
+                            }
+                            composable(settingsTab.title) {
+                                Text(settingsTab.title)
+                            }
+                        }
+                    }
+                    }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ISENSmartCompanionTheme {
-        Greeting("Android")
+        MainScreen(PaddingValues(8.dp))
     }
 }
---
+++ app/src/main/java/fr/isen/fayet/isensmartcompanion/screen/EventsScreen.kt
@@ -0,0 +1,8 @@
+package fr.isen.fayet.isensmartcompanion.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun EventsScreen() {
+
+}
